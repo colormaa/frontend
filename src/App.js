9accb3f5355865389f43c8eb055d2a3fd25ee8d5
colormaa <colormaa@gmail.com>
2022-09-28 08:12:46 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         O yeah reac I chanaged 
         </a>
       </header>
     </div>
